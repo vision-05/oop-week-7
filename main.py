a2b9cc26474f2d2d18133cca797348b67bac29c7
Aayan islam <zcabais@ucl.ac.uk>
2025-11-14 14:10:21 +0000

    daily to monthly stub

--- main.py
+++ main.py
@@ -3,3 +3,9 @@ import pandas as pd
 def daily_entries_2_to_monthly(filename):
     """Take daily entries 2 and convert to monthly entry"""
     pass
+
+def daily_to_monthly (filename = "dataflow.csv" , outfile = "monthly_balances.csv"):
+    # load the daily entries csv file 
+    df = pd.read_csv(filename)
+    df.groupby("Month")["Balance"].sum()
+    print(df.head())
