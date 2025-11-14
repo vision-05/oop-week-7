3db9fc3a8b66ba72b56e5a132cc0fdd6b7efd928
vision-05 <t.frankel@outlook.com>
2025-11-14 14:46:35 +0000

    fixing code

--- main.py
+++ main.py
@@ -2,17 +2,21 @@ import pandas as pd
 
 def daily_entries_2_to_monthly():
     """Take daily entries 2 and convert to monthly entry"""
-    daily_to_monthly("daily_entries_2.csv", "monthly_balances.csv")
-    daily1 = pd.read_csv("daily_entries_1.csv")
-    daily2 = pd.read_csv("daily_entries_2.csv")
-    pd.merge()
+    #daily_to_monthly("dataflow.csv", "monthly_balances.csv")
+    daily1 = pd.read_csv("dataflow.csv")
+    daily2 = pd.read_csv("daily_2.csv")
+    print(daily1)
+    print(daily2)
+    daily1.merge(daily2, on="Customer_Name", how="outer")
+    daily_tot = daily1.groupby(["Customer_Id","Month", "Day"]).sum()
+    print(daily_tot)
     pass
 
 def daily_to_monthly (filename = "dataflow.csv" , outfile = "monthly_balances.csv"):
     # load the daily entries csv file 
     df = pd.read_csv(filename)
     print(df)
-    df.groupby("Month")["Balance"].sum()
+    df.groupby(by=["Month"], as_index=False)
     print(df.head())
 
-daily_to_monthly()
+daily_entries_2_to_monthly()
