e7c970a8a439bce329a72dd8e1b81edbc72c2f03
vision-05 <t.frankel@outlook.com>
2025-11-14 14:13:21 +0000

    started entries 2 monthly

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import pandas as pd
 
-def daily_entries_2_to_monthly(filename):
+def daily_entries_2_to_monthly():
     """Take daily entries 2 and convert to monthly entry"""
-    pass
+    daily_to_monthly("daily_entries_2.csv", "monthly_balances.csv")
+    daily1 = pd.read_csv("daily_entries_1.csv")
+    daily2 = pd.read_csv("daily_entries_2.csv")
+    pd.merge()
