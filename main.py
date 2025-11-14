2a77841311102b3af353342a5ce3c8628f33eadd
Aryan Patel <aryanpatel@Aryans-MacBook-Pro.local>
2025-11-14 13:56:46 +0000

    template funcions

---
+++ main.py
@@ -0,0 +1,25 @@
+def plot_all_customer_entries():
+    """ Function to plots all the entries of the customer given customer name or ID"""
+    pass
+
+
+def plot_entries_on_day():
+    """ Function to plot all entries on the day when given a day of the year"""
+    pass
+
+
+def plot_customer_balance():
+    """ Function to plot balance of the customer per month (in grouped column bars)"""
+    pass
+
+
+
+def plot_all_entries():
+    """ Function to plot all the entires within a period per customer"""
+    pass
+
+
+
+def plot_negative_transactions():
+    """ Function to plot all negative transactions within period"""
+    pass
